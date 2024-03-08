422ae117c74d2e7c869aeebfc35394aaeaab1977
Shannu1881 <109470585+Shannu1881@users.noreply.github.com>
2024-03-08 18:46:45 +0530

    forms and table data

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from "react";
 
 import Class1 from './Component/Class1';
 import FirstClass from "./class-com/Class1";
+import Class2 from './Component/Class2';
+import Class3 from './Component/Class3';
+import Class4 from './Component/Class4';
+
 import Navgation from "./Component/Navgation";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Class2 from "./Component/Class2";
-import Class3 from "./Component/Class3";
-import Class4 from "./Component/Class4";
 
 function App(){
   return(
    <>
    <BrowserRouter>
-     <Navgation />
+   <Navgation />
      <Routes>
-        <Route path="/" element={ <Class1 /> } />
-        <Route path="/class2" element={ <Class2 /> } />
-        <Route path="/class3" element={ <Class3 /> } />
-        <Route path="/class4" element={ <Class4 /> } />
+       <Route path="/" element={<Class1 />} />
+       <Route path="/class2" element={<Class2 />} />
+       <Route path="/class3" element={<Class3 />} />
+       <Route path="/class4" element={<Class4 />} />
      </Routes>
    </BrowserRouter>
    
--- src/Component/Class2.jsx
+++ src/Component/Class2.jsx
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Class2 = () => {
+
+  const [username, setUsername] = useState('');
+  const [mobile, setMobile] = useState();
+  const [password, setPassword] = useState("");
+
+  const [userList, setUserList] = useState([]);
+
+  const sendData = (e) =>{
+    e.preventDefault();
+    console.log(username);
+    console.log(mobile);
+    console.log(password);
+  }
+
+  const getData = async () =>{
+   let res = await fetch('https://jsonplaceholder.typicode.com/users');
+   let data = await res.json();
+   console.log(data);
+   setUserList(data);
+  }
+
+  useEffect(()=>{
+    getData();
+  })
+
   return (
-    <div>Class2</div>
+    <div>
+      <h2> Login Page</h2>
+      <div>
+        <form action="">
+          <label htmlFor="">Username</label> <br />
+          <input type="text" name="" id="" onChange={(obj)=>{ setUsername(obj.target.value) }} /> <br />
+          <label htmlFor="">Mobile</label> <br />
+          <input type="number" name="" id="" onChange={(e)=>{ setMobile(e.target.value) }} /> <br />
+          <label htmlFor="">Password</label> <br />
+          <input type="password" name="" id="" onChange={(e)=>{ setPassword(e.target.value) }}/> <br /><br />
+          <button onClick={sendData}> Submit </button>
+        </form>
+      </div>
+
+      <div>
+         <table border={1} style={{borderCollapse:'collapse', width:'60%'}}>
+          <thead>
+            <tr>
+              <th>Sno</th>
+              <th>Name</th>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              userList.map((item, index)=>{
+                return(
+                  <tr key={index} >
+                    <td> {item.id} </td>
+                    <td> {item.name} </td>
+                    <td> {item.username} </td>
+                    <td> {item.email} </td>
+                    <td> {item.address.city} </td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+         </table>
+      </div>
+    </div>
   )
 }
 
